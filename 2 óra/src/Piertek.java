490d0a668df4c31c5173c5bcd861fd08c7ba3772
Nexus <Nexus@192.168.3.109>
2018-02-21 13:39:50 +0100

    OOP gyak leibniz

--- 2 óra/src/Piertek.java
+++ 2 óra/src/Piertek.java
@@ -7,8 +7,8 @@ public class Piertek {
 		double darab = 1000;
 		
 		for (double i = 1; i<= darab; i++){
-			pi *= 2 *i / (2 * i - 1);
-			pi *= 2 *i / (2 * i + 1);
+			pi *= 2.0 *i / (2 * i - 1);
+			pi *= 2.0 *i / (2 * i + 1);
 			
 		}
 		
@@ -16,6 +16,18 @@ public class Piertek {
 		System.out.println("pi/2 értéke: "+ pi);
 		System.out.println("pi: " + pi *2);
 		
+		//Leibniz féle sor
+		
+		pi = 0;
+		darab *= 4;
+		int elojel = 1;
+		for (double i=1; i<=darab; i +=2){
+			pi += elojel * 1.0 / i;
+			elojel *= elojel * -1;
+		}
+		System.out.println("Leibniz féle sor: ");
+		System.out.println("pi/4 értéke: "+ pi);
+		System.out.println("pi: " + pi *4);
 		
 	}
 
